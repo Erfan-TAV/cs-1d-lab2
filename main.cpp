8e6150f0aa76355c43de8c221d8182c13a135949
Erfan <106490629+Erfan-TAV@users.noreply.github.com>
2026-01-14 16:30:03 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,16 @@
-//
-// Created by Erfan Tavassoli on 1/14/26.
-//
-
-// Shahob Shahmirzadi and Erfan
-// Description: Hello World in C++
-//============================================================
 #include <iostream>
+#include "inherit2.h"
+#include "partTimeEmployee.h"
 using namespace std;
-int hello1Function(string name1);
-int hello2Function(string name2);
 int main()
 {
-  string name1="Erfan Tavassoli";
-  hello1Function(name1);
-  string name2="Shahob Shahmirzadi";
-  hello2Function(name2);
+  personType newPerson;
+  partTimeEmployee newEmployee("John","Smith",7.50,56);
+  partTimeEmployee employee;
+  
+  newEmployee.print();
+  employee.setNameRateHours("Rachel", "Moore",9.75, 45);
+  employee.print();
+  
   return 0;
 }
