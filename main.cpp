fb4f976e21bcd6ed669733a0dc586419eb64bc37
ShahobCode <shahob.coder@gmail.com>
2026-01-14 16:15:22 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 //
 // Created by Erfan Tavassoli on 1/14/26.
+//
 
-
-// this is the branch
-
+// Shahob Shahmirzadi and Erfan
+// Description: Hello World in C++
+//============================================================
+#include <iostream>
+using namespace std;
+int hello1Function(string name1);
+int hello2Function(string name2);
+int main()
+{
+  string name1="your name1";
+  hello1Function(name1);
+  string name2="your name2";
+  hello2Function(name2);
+  return 0;
+}
